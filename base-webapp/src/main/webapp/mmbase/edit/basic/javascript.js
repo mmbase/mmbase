2e72fe2bc633f9d57076ca6ba0bec2aa177283cc
André van Toly <andre@toly.nl>
2025-12-21 12:27:40 +0100

    cleanup

--- base-webapp/src/main/webapp/mmbase/edit/basic/javascript.js
+++ base-webapp/src/main/webapp/mmbase/edit/basic/javascript.js
@@ -1,37 +1,31 @@
 var validator;
 
 (function () {
-    console.log('do we run?!');
-    Widgets.instance.enumerationSuggestion("body.config select[name=mmjspeditors_uri]");
-    domReady(function () {
-        validator = new MMBaseValidator();
-        validator.logEnabled = false;
-        validator.traceEnabled = false;
-        validator.validateHook = function () {
-            var okbutton = document.getElementById("okbutton");
-            if (okbutton != null) okbutton.disabled = this.invalidElements != 0;
-            var savebutton = document.getElementById("savebutton");
-            if (savebutton != null) savebutton.disabled = this.invalidElements != 0;
-        };
+	Widgets.instance.enumerationSuggestion("body.config select[name=mmjspeditors_uri]");
 
-        // validator.lang = $("html head meta[name='MMBase-Language']").attr("content");
-        // validator.sessionName = $("html head meta[name='MMBase-SessionName']").attr("content");
-        // var nt = $("html head meta[name='MMBase-NodeType']").attr("content");
-        validator.lang =
-            document.querySelector("html head meta[name='MMBase-Language']").getAttribute("content") || "en";
-        validator.sessionName = document
-            .querySelector("html head meta[name='MMBase-SessionName']")
-            .getAttribute("content");
-        var nt = document.querySelector("html head meta[name='MMBase-NodeType']").getAttribute("content");
+	validator = new MMBaseValidator();
+	validator.logEnabled = false;
+	validator.traceEnabled = false;
+	validator.validateHook = function () {
+		var okbutton = document.getElementById("okbutton");
+		if (okbutton != null) {
+			okbutton.disabled = this.invalidElements != 0;
+		}
+		var savebutton = document.getElementById("savebutton");
+		if (savebutton != null) {
+			savebutton.disabled = this.invalidElements != 0;
+		}
+	};
 
-        if (nt != null && nt.length > 0) {
-            validator.prefetchNodeManager(nt);
-        }
-        const forms = document.querySelectorAll("form[name=change], form[name=create]");
-        forms.forEach((form) => {
-            validator.addValidation(form);
-        });
-        // validator.addValidation($("form[name=change]"));
-        // validator.addValidation($("form[name=create]"));
-    });
+	validator.lang = document.querySelector("html head meta[name='MMBase-Language']")?.getAttribute("content") || "en";
+	validator.sessionName = document.querySelector("html head meta[name='MMBase-SessionName']")?.getAttribute("content");
+	var nt = document.querySelector("html head meta[name='MMBase-NodeType']")?.getAttribute("content");
+
+	if (nt != null && nt.length > 0) {
+		validator.prefetchNodeManager(nt);
+	}
+	var forms = document.querySelectorAll("form[name=change], form[name=create]");
+	forms.forEach((form) => {
+		validator.addValidation(form);
+	});
 })();
