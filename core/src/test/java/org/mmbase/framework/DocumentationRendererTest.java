68ee143e9f497b226c11a718bbf2ae157b39199e
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2026-03-04 13:30:36 +0100

    https://github.com/mmbase/mmbase/issues/22

--- core/src/test/java/org/mmbase/framework/DocumentationRendererTest.java
+++ core/src/test/java/org/mmbase/framework/DocumentationRendererTest.java
@@ -9,6 +9,8 @@ import org.mmbase.bridge.CloudContext;
 import org.mmbase.bridge.mock.MockCloudContext;
 import org.mmbase.datatypes.DataTypes;
 import org.mmbase.util.functions.Parameters;
+import static org.junit.Assert.*;
+
 
 public class DocumentationRendererTest {
 
@@ -31,6 +33,18 @@ public class DocumentationRendererTest {
         StringWriter writer = new StringWriter();
         renderer.render(Parameters.VOID, writer, new RenderHints(renderer, WindowState.MAXIMIZED, "1", "clazz", RenderHints.Mode.NORMAL));
         System.out.println(writer);
+    }
+
+
+    @Test
+    public void url() {
+        DocumentationRenderer renderer = new DocumentationRenderer(Renderer.Type.BODY, new Block("test", "text/html", new BasicComponent("bla"), "cla"));
+        renderer.setProject("mmbase");
+        renderer.setModule("contributions/lucene");
+        renderer.setDocbook("documentation/lucene.xml");
+        assertEquals(renderer.getUri().toString(), "https://raw.githubusercontent.com/mmbase/mmbase/refs/heads/main/contributions/lucene/documentation/lucene.xml");
+
+
     }
 
 }
